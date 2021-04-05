c092b215f2ff72ccda2876291987471543840774
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-05 00:46:45 -0700

    add vex.ColorHue stubs

--- vex/__init__.py
+++ vex/__init__.py
@@ -112,11 +112,24 @@ class Bumper:
     ...
 
 
-class ColorHue:
+class ColorHue(Enum):
     """
     Defined color hue values.
     """
-    ...
+    NONE: int = 0
+    RED: int = 1
+    RED_ORANGE: int = 2
+    ORANGE: int = 3
+    YELLOW_ORANGE: int = 4
+    YELLOW: int = 5
+    YELLOW_GREEN: int = 6
+    GREEN: int = 7
+    BLUE_GREEN: int = 8
+    BLUE: int = 9
+    BLUE_VIOLET: int = 10
+    VIOLET: int = 11
+    RED_VIOLET: int = 12
+    WHITE: int = 13
 
 
 class Colorsensor:
