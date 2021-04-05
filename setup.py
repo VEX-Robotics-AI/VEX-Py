ced29b11701cb13692c7c4ceac2a3c19fc85264c
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-04 23:13:16 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_namespace_packages, setup
+
+
+setup(
+    name='RobotMesh-VEXIQ-PyB-API-Stubs',
+    version='0.0.0.dev0',
+    description='Robot Mesh VEX IQ Python B API Stubs',
+    long_description='Robot Mesh VEX IQ Python B API Stubs',
+    author='STEAM for Vietnam Foundation',
+    author_email='Edu.Robotics@STEAMforVietnam.org',
+    url='https://GitHub.com/STEAMforVietnam/RobotMesh-VEXIQ-PyB-API-Stubs',
+    download_url='https://GitHub.com/STEAMforVietnam/'
+                 'RobotMesh-VEXIQ-PyB-API-Stubs/archive/main.zip',
+    packages=find_namespace_packages()
+)
