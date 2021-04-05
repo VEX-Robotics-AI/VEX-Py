05c22cff1e32e856ec09855ed05f0fd9ea4af0a7
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-05 00:56:52 -0700

    add vex.Ports stubs

--- vex/__init__.py
+++ vex/__init__.py
@@ -216,8 +216,19 @@ class NoteType(Enum):
     B: int = 7
 
 
-class Ports:
-    ...
+class Ports(Enum):
+    PORT1: int = 0
+    PORT2: int = 1
+    PORT3: int = 2
+    PORT4: int = 3
+    PORT5: int = 4
+    PORT6: int = 5
+    PORT7: int = 6
+    PORT8: int = 7
+    PORT9: int = 8
+    PORT10: int = 9
+    PORT11: int = 10
+    PORT12: int = 11
 
 
 class RotationUnits:
