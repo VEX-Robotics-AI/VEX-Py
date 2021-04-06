4c620742b0cc21c526c1b1a2dfb9f355c72c3846
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-05 21:51:10 -0700

    factor out vex.color_sensor

--- vex/__init__.py
+++ vex/__init__.py
@@ -12,31 +12,8 @@ from .abstract import Device, Enum
 from .port import Ports
 from .brain import Brain, BrainButton, BrainLcd, BrainSound
 from .bumper_switch_sensor import Bumper
+from .color_sensor import ColorHue, Colorsensor
 from .motor import BrakeType, Motor
-
-
-class ColorHue(Enum):
-    """
-    Defined color hue values.
-    """
-    NONE: int = 0
-    RED: int = 1
-    RED_ORANGE: int = 2
-    ORANGE: int = 3
-    YELLOW_ORANGE: int = 4
-    YELLOW: int = 5
-    YELLOW_GREEN: int = 6
-    GREEN: int = 7
-    BLUE_GREEN: int = 8
-    BLUE: int = 9
-    BLUE_VIOLET: int = 10
-    VIOLET: int = 11
-    RED_VIOLET: int = 12
-    WHITE: int = 13
-
-
-class Colorsensor(Device):
-    ...
 
 
 class Controller(Device):
