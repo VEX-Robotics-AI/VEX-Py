07f4dc71c01412a28ad990c8ec3eb29efba554cf
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-06 14:08:02 -0700

    refactor drivetrain module

--- drivetrain/__init__.py
+++ drivetrain/__init__.py
@@ -1,24 +1,32 @@
-from typing import Optional
+from typing import Optional, TypeVar
 
+from motor_group import MotorGroup
 from vex import (
+    BrakeType,
     DirectionType,
     DistanceUnits,
     Motor,
     RotationUnits,
     TimeUnits,
+    TurnType,
     VelocityUnits
 )
+
+
+DrivetrainMotorType = \
+    TypeVar('DrivetrainMotorType',
+            Motor, MotorGroup, list[MotorGroup], tuple[Motor])
 
 
 class Drivetrain:
     def __init__(
             self,
-            left_motor: Motor,
-            right_motor: Motor,
-            wheel_travel: int = 200,
-            track_width: int = 176,
+            left_motor: DrivetrainMotorType,
+            right_motor: DrivetrainMotorType,
+            wheel_travel: float = 200,
+            track_width: float = 176,
             distanceUnits: DistanceUnits = DistanceUnits.MM,
-            gear_ratio: float = 1.0):
+            gear_ratio: float = 1):
         """
         Creates a new drivetrain object.
 
@@ -41,6 +49,13 @@ class Drivetrain:
             velocityUnits: VelocityUnits = VelocityUnits.PCT):
         """
         Turns the motors on and drives in the specified direction.
+
+        Parameters:
+        - directionType: direction to drive in, forward or reverse,
+                         a DirectionType enum value
+        - velocity: set velocity of the motors
+        - velocityUnits: unit for the velocity parameter,
+                         a VelocityUnits enum value
         """
 
     def drive_for(
@@ -48,87 +63,229 @@ class Drivetrain:
             directionType: DirectionType,
             distance: float,
             distanceUnits: DistanceUnits = DistanceUnits.MM,
-            velocity: int =None, velocityUnits=VelocityUnits.PCT, waitForCompletion=True):
+            velocity: Optional[float] = None,
+            velocityUnits: VelocityUnits = VelocityUnits.PCT,
+            waitForCompletion: bool = True) -> bool:
         """
         Drives for a specified distance.
+
+        Parameters
+        - directionType: direction to drive in, forward or reverse,
+                         a DirectionType enum value
+        - distance: distance to drive in
+        - distanceUnits: unit for the distance parameter,
+                         a DistanceUnits enum value
+        - velocity: set velocity of the motors
+        - velocityUnits: unit for the velocity parameter,
+                         a VelocityUnits enum value
+        - waitForCompletion: if True, your program will wait until
+                             the motor reaches the target rotational value,
+                             otherwise it will continue immediately.
+
+        Returns:
+        True if the drivetrain has reached the target distance, False otherwise
         """
 
-    def start_drive_for(self, directionType: DirectionType, distance, distanceUnits: DistanceUnits = DistanceUnits.MM, velocity: int =None, velocityUnits=VelocityUnits.PCT):
+    def start_drive_for(
+            self,
+            directionType: DirectionType,
+            distance: float,
+            distanceUnits: DistanceUnits = DistanceUnits.MM,
+            velocity: Optional[float] = None,
+            velocityUnits: VelocityUnits = VelocityUnits.PCT):
         """
         Start driving for a specified distance.
+
+        Parameters:
+        - directionType: direction to drive in, forward or reverse,
+                         a DirectionType enum value
+        - distance: distance to drive in
+        - distanceUnits: unit for the distance parameter,
+                         a DistanceUnits enum value
+        - velocity: set velocity of the motors
+        - velocityUnits: unit for the velocity parameter,
+                         a VelocityUnits enum value
         """
 
-    def turn(self, turnType, velocity: int =None, velocityUnits=VelocityUnits.PCT):
+    def turn(
+            self,
+            turnType: TurnType,
+            velocity: Optional[float] = None,
+            velocityUnits: VelocityUnits = VelocityUnits.PCT):
         """
         Turn the drivetrain left or right.
+
+        Parameters:
+        - turnType: direction to turn in, left or right, a TurnType enum value
+        - velocity: set velocity of the motors
+        - velocityUnits: unit for the velocity parameter,
+                         a VelocityUnits enum value
         """
 
-    def turn_for(self, turnType, angle, rotationUnits=RotationUnits.DEG, velocity: int =None, velocityUnits=VelocityUnits.PCT, waitForCompletion=True):
+    def turn_for(
+            self,
+            turnType: TurnType,
+            angle: float,
+            rotationUnits: RotationUnits = RotationUnits.DEG,
+            velocity: Optional[float] = None,
+            velocityUnits: VelocityUnits = VelocityUnits.PCT,
+            waitForCompletion: bool = True) -> bool:
         """
         Turn the drivetrain left or right until the specified angle is reached.
+
+        Parameters:
+        - turnType: direction to turn in, left or right, a TurnType enum value
+        - angle: sets the angle to turn
+        - rotationUnits: units for the angle parameter,
+                         a RotationUnits enum value
+        - velocity: set velocity of the motors
+        - velocityUnits: unit for the velocity parameter,
+                         a VelocityUnits enum value
+        - waitForCompletion: if True, your program will wait until
+                             the motor reaches the target rotational value,
+                            otherwise it will continue immediately.
+
+        Returns:
+        True if the drivetrain has reached the target angle, False otherwise
+
+        Reimplemented in smartdrive.Smartdrive.
         """
 
-    def start_turn_for(self, turnType, angle, angleUnits=RotationUnits.DEG, velocity: int =None, velocityUnits=VelocityUnits.PCT):
+    def start_turn_for(
+            self,
+            turnType: TurnType,
+            angle: float,
+            angleUnits: RotationUnits = RotationUnits.DEG,
+            velocity: Optional[float] = None,
+            velocityUnits: VelocityUnits = VelocityUnits.PCT):
         """
-        Start turning the drivetrain left or right unitl the specified angle is reached.
+        Start turning the drivetrain left or right
+        until the specified angle is reached.
+
+        Parameters:
+        - turnType: direction to turn in, left or right, a TurnType enum value
+        - angle: sets the angle to turn
+        - angleUnits: units for the angle parameter, a RotationUnits enum value
+        - velocity: set velocity of the motors
+        - velocityUnits: unit for the velocity parameter,
+                         a VelocityUnits enum value
+
+        Reimplemented in smartdrive.Smartdrive.
         """
 
-    def arcade(self, drivePower: int, turnPower: int):
+    def arcade(self, drivePower: float, turnPower: float):
         """
-        Drive in arcade mode, normally corresponding to two controller joystick axis values.
+        Drive in arcade mode,
+        normally corresponding to two controller joystick axis values.
+
+        Parameters:
+        - drivePower: percent power to apply to driving, -100..100
+        - turnPower: percent power to apply to turning, -100..100
         """
 
-    def stop(self, brakeType=None):
+    def stop(self, brakeType: Optional[BrakeType] = None):
         """
         Stops the drive using a specified brake mode.
+
+        Parameters:
+        - brakeType: brake mode, an BrakeType enum value.
+                     If omitted, the value set in set_stopping is used.
         """
 
     def set_gear_ratio(self, gear_ratio):
         """
         Sets the external gear ratio of the drivetrain.
+
+        Parameters:
+        - gear_ratio: gear ratio value, usually 1.0
         """
 
     def set_drive_velocity(self, velocity, velocityUnits=VelocityUnits.PCT):
         """
         Sets the velocity of the drive.
+
+        Will not run the motors.
+        Any subsequent call that does not contain a specified velocity
+        will use this value.
+
+        Parameters:
+        - velocity: Sets the amount of velocity.
+        - velocityUnits: unit for the velocity parameter,
+                         a VelocityUnits enum value
         """
 
-    def set_turn_velocity(self, velocity, velocityUnits=VelocityUnits.PCT):
+    def set_turn_velocity(
+            self,
+            velocity: float,
+            velocityUnits: VelocityUnits = VelocityUnits.PCT):
         """
         Sets the velocity of the turn.
+
+        Will not run the motors.
+
+        Parameters:
+        - velocity: Sets the amount of velocity.
+        - velocityUnits: unit for the velocity parameter,
+                         a VelocityUnits enum value
         """
 
-    def set_timeout(self, time, timeUnits=TimeUnits.SEC):
+    def set_timeout(self, time: float, timeUnits: TimeUnits = TimeUnits.SEC):
         """
         Sets the timeout for the drivetrain.
+
+        If the drivetrain does not reach its' commanded position
+        prior to the completion of the timeout, the motors will stop.
+
+        Parameters:
+        - time: the amount of time.
+        - timeUnits: unit for the time parameter, a TimeUnits enum value
         """
 
-    def timeout(self, timeUnits=TimeUnits.SEC):
+    def timeout(self, timeUnits: TimeUnits = TimeUnits.SEC):
         """
         Returns a timeout in given time units.
         """
 
-    def did_timeout(self):
+    def did_timeout(self) -> bool:
         """
         True if the last drivetrain operation timed out, False otherwise.
         """
 
-    def is_done(self):
+    def is_done(self) -> bool:
         """
-        True if drivetrain is done driving/turning to a specified target, False otherwise.
+        True if drivetrain is done driving/turning to a specified target,
+        False otherwise.
+
+        Reimplemented in smartdrive.Smartdrive.
         """
 
-    def set_stopping(self, brakeType):
+    def set_stopping(self, brakeType: BrakeType):
         """
-        Sets the stopping mode of the motor group by passing a brake mode as a parameter.
+        Sets the stopping mode of the motor group
+        by passing a brake mode as a parameter.
+
+        Parameters:
+        - brakeType: the stopping mode, a BrakeType enum value
+                     (coast, brake, or hold).
         """
 
-    def velocity(self, velocityUnits=VelocityUnits.PCT):
+    def velocity(
+            self,
+            velocityUnits: VelocityUnits = VelocityUnits.PCT) -> float:
         """
         Gets the average current velocity of all motors.
+
+        Returns:
+        a float that represents the average current velocity
+
+        Parameters:
+        - velocityUnits: The measurement unit for the velocity.
         """
 
-    def current(self):
+    def current(self) -> float:
         """
         Gets the electrical current of all motors.
+
+        Returns:
+        a float that represents the electrical current of the motor in Amps.
         """
