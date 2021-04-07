5df42f695b873296db0d53afa599fe76fc6e7081
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-07 10:26:41 -0700

    refactor __decor.act(...) & .sense(...) decorators

--- __decor/__init__.py
+++ __decor/__init__.py
@@ -36,12 +36,17 @@ def act(actuating_func: CallableTypeVar) -> CallableTypeVar:
 
     @wraps(actuating_func)
     def decor_actuating_func(*given_args):
-        result = (actuating_func.__qualname__,
-                  args_dict_from_func_and_given_args(
-                    func=actuating_func,
-                    given_args=given_args))
-        print(result)
-        return result
+        args_dict = args_dict_from_func_and_given_args(
+                        func=actuating_func,
+                        given_args=given_args)
+
+        print_args = args_dict.copy()
+        self_arg = print_args.pop('self', None)
+        input_arg_strs = [f'{k}={v}' for k, v in print_args.items()]
+        print(('ACT: ' if self_arg is None else f'ACT: {self_arg}.') +
+              f"{actuating_func.__name__}({', '.join(input_arg_strs)})")
+
+        return (actuating_func.__qualname__, args_dict)
 
     return decor_actuating_func
 
@@ -56,7 +61,7 @@ def sense(sensing_func: CallableTypeVar) -> CallableTypeVar:
                         given_args=given_args)
         self_arg = args_dict.pop('self')
         input_arg_strs = [f'{k}={v}' for k, v in args_dict.items()]
-        return json.loads(input(f'{self_arg}.{sensing_func.__name__}'
+        return json.loads(input(f'SENSE: {self_arg}.{sensing_func.__name__}'
                                 f"({', '.join(input_arg_strs)}) = ?   "))
 
     return decor_sensing_func
