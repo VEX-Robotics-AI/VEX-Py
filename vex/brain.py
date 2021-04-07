a0db68a403a4b18c2581a433f4ea7ccf11bd6f7b
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-07 00:03:59 -0700

    add vex.brain.BrainSound

--- vex/brain.py
+++ vex/brain.py
@@ -5,6 +5,7 @@ from enum import IntEnum
 from __decor import return_qualname_and_args
 
 from .abstract import DeviceWithoutPort
+from .time import TimeUnits
 
 
 @return_qualname_and_args
@@ -91,7 +92,81 @@ class BrainLcd(DeviceWithoutPort):
 
 @return_qualname_and_args
 class BrainSound(DeviceWithoutPort):
-    ...
+    def play(
+            self,
+            note: NoteType,
+            octave: int = 3,
+            duration: float = 0.5,
+            timeUnits: TimeUnits = TimeUnits.SEC):
+        """
+        Play a musical note on the speaker.
+
+        Parameters:
+        - note: musical note to play: NoteType enum value
+        - octave: octave of the note [1-7], optional
+        - duration: time. 0 to start playing without blocking. Default 0.5
+        - timeUnits: of time for the duration. Default sec.
+        """
+
+    def play_raw(
+            self,
+            note: NoteType,
+            duration: float = 0.5,
+            timeUnits: TimeUnits = TimeUnits.SEC):
+        """
+        Play a musical note on the speaker.
+
+        Parameters
+        - note: musical note to play: 0=silence/stop, [1-56] numeric value
+        - duration: time. 0 to start playing without blocking. Default 0.5
+        - timeUnits: of time for the duration. Default sec.
+        """
+
+    def play_wave(
+            self,
+            waveType: int,
+            waitForCompletion: bool = True):
+        """
+        play the wave sample
+
+        Parameters:
+        - waveType: type of the wave sample sound to play [0..15]
+        - waitForCompletion: wait for the sample to finish playing
+        """
+
+    def play_melody(self, melody: str):
+        """
+        Play a melody form a string in a quasi musical alphabet notation
+        (cdefgab)
+
+        Parameters:
+        - melody: string [cdefgab]: musical alphabet for notes,
+                  space: pause,
+                  +/-: increase/decrease octave of following notes
+                  0-9: set duration of following notes
+                       (in 1/8s: 1=eighth note...8 = full note)
+        """
+
+    def set_sound_effect(self, effect: int):
+        """
+        set the sound effect type for subsequent notes played
+
+        Parameters
+        - effect: effect type [0..15]
+        """
+
+    def set_volume(self, volume: int):
+        """
+        set the sound volume [1-4]
+
+        Parameters
+        - volume: value [1=low...4=high]
+        """
+
+    def stop(self):
+        """
+        Stop playing music.
+        """
 
 
 class NoteType(IntEnum):
