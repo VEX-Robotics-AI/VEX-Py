2210a31ae961036176f3e1673e22ee8b63306473
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-06 22:38:58 -0700

    enhance __decor.return_qualname_and_args

--- __decor/__init__.py
+++ __decor/__init__.py
@@ -1,23 +1,33 @@
+from inspect import getfullargspec, getmembers, isclass, isfunction
 from typing import Any, Callable, TypeVar
 
 
 CallableTypeVar = TypeVar('CallableTypeVar', bound=Callable[..., Any])
 
 
-def return_qualname_and_args(func: CallableTypeVar) -> CallableTypeVar:
-    # (use same func type var for IDE code autocomplete to work)
+def return_qualname_and_args(cls_or_func: CallableTypeVar) -> CallableTypeVar:
+    # (use same signature for IDE code autocomplete to work)
 
-    def decor_func(*args):
-        d = func.__annotations__.copy()
-        n_args = len(d)
+    def decor_func(*given_args):
+        arg_spec = getfullargspec(cls_or_func)
+        arg_names = arg_spec.args
+        n_defaults_to_use = len(arg_names) - len(given_args)
+        result = (cls_or_func.__qualname__,
+                  {arg_names[i]: v for i, v in enumerate(given_args)} |
+                  dict(zip(arg_names[-n_defaults_to_use:],
+                           arg_spec.defaults[-n_defaults_to_use:])))
+        print(result)
+        return result
 
-        if len(args) == n_args:   # if normal function
-            return (func.__qualname__,
-                    {k: args[i] for i, k in enumerate(d)})
+    if isfunction(cls_or_func):
+        return decor_func
 
-        else:
-            assert len(args) == n_args + 1   # else must be class method
-            return (func.__qualname__,
-                    {k: args[i + 1] for i, k in enumerate(d)})
+    else:
+        assert isclass(cls_or_func)
 
-    return decor_func
+        for method_name, method in getmembers(cls_or_func, isfunction):
+            if not method_name.startswith('_'):
+                setattr(cls_or_func, method_name,
+                        return_qualname_and_args(method))
+
+        return cls_or_func
