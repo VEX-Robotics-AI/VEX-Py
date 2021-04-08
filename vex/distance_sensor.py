d2c209e628524e60e3c2c8b7bbc659ba0ef4eca8
Thanh Nguyen <tnguyen@steamforvietnam.org>
2021-04-08 13:47:06 +0700

    add Sonar stubs

--- vex/distance_sensor.py
+++ vex/distance_sensor.py
@@ -4,4 +4,32 @@ from .abstract import Device
 
 
 class Sonar(Device):
-    ...
+    def __init__(self, index):
+        """
+        Creates a new sonar sensor object on the port specified in the parameter.
+
+        Params:
+        index: to the brain port.
+        """
+
+    @act
+    def set_maximum(self, distance, distanceUnits=DistanceUnits.MM):
+        """
+        Sets the maximum distance (default 2.5m)
+
+        Params:
+        distance: maximum distance to be measured in units
+        distanceUnits: a DistanceUnits enum value for the measurement unit.
+        """"
+
+    @property
+    def distance(self, distanceUnits=DistanceUnits.MM):
+        """
+        Gets the value of the sonar sensor.
+
+        Params:
+        distanceUnits: The measurement unit for the sonar device, DistanceUnits enum value.
+
+        Returns:
+        an integer that represents the unit value specified by the parameter.
+        """"
