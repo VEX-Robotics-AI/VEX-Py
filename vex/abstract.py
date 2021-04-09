b5107162c0e8295725fe3be5ae0af90639149058
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-09 13:24:58 -0700

    minor edit to vex.abstract.Device.__str__

--- vex/abstract.py
+++ vex/abstract.py
@@ -14,7 +14,7 @@ class Device:
         self._port = port
 
     def __str__(self) -> str:
-        return f'{type(self).__name__}({str(self.port)})'
+        return f'{type(self).__name__}({self.port.name})'
 
 
 class SingletonDevice:
