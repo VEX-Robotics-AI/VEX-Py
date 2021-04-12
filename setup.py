5267efb91881d59604e6114c106dda1b04866768
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-12 15:45:12 -0700

    refactor py modules

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 setup(
     name='RobotMesh-VEXIQ-PyB-API-Stubs',
-    version='1.0.0',
+    version='1.0.1',
     description='Robot Mesh VEX IQ Python B API Stubs',
-    long_description='Robot Mesh VEX IQ Python B API Stubs',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='STEAM for Vietnam Foundation',
     author_email='Edu.Robotics@STEAMforVietnam.org',
     url='https://GitHub.com/STEAMforVietnam/RobotMesh-VEXIQ-PyB-API-Stubs',
     download_url='https://GitHub.com/STEAMforVietnam/'
                  'RobotMesh-VEXIQ-PyB-API-Stubs/archive/main.zip',
-    packages=find_packages(exclude=('tests',))
+    packages=['vex'],
+    py_modules=['drivetrain', 'motor_group', 'smartdrive', 'timer', 'vision',
+                '__decor'],
+    scripts=[],
+    classifiers=[],
+    license='MIT',
+    keywords=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=open('requirements.txt').readlines(),
+    entry_points={},
+    extras_require={},
+    python_requires='>= 3.8',
+    setup_requires=[],
+    namespace_packages=[]
 )
