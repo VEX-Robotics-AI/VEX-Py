3ae0f7bf9cc3fb1f9e40cbfc7da00d1824d8c64a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-22 14:31:13 -0700

    enhance __decor.sense wrapper

--- src/__decor.py
+++ src/__decor.py
@@ -20,13 +20,11 @@ def args_dict_from_func_and_given_args(func, given_args):
     arg_spec = getfullargspec(func)
     arg_names = arg_spec.args
 
-    args_dict = {arg_names[i]: stringify_device_or_enum(v)
-                 for i, v in enumerate(given_args)}
+    args_dict = {arg_names[i]: v for i, v in enumerate(given_args)}
     if (n_defaults_to_use := len(arg_names) - len(given_args)) > 0:
         args_dict.update(
             zip(arg_names[-n_defaults_to_use:],
-                map(stringify_device_or_enum,
-                    arg_spec.defaults[-n_defaults_to_use:])))
+                arg_spec.defaults[-n_defaults_to_use:]))
 
     return args_dict
 
@@ -42,7 +40,8 @@ def act(actuating_func: CallableTypeVar) -> CallableTypeVar:
 
         print_args = args_dict.copy()
         self_arg = print_args.pop('self', None)
-        input_arg_strs = [f'{k}={v}' for k, v in print_args.items()]
+        input_arg_strs = [f'{k}={stringify_device_or_enum(v)}'
+                          for k, v in print_args.items()]
         print((f'ACT: {self_arg}.' if self_arg else 'ACT: ') +
               f"{actuating_func.__name__}({', '.join(input_arg_strs)})")
 
@@ -77,11 +76,17 @@ def sense(sensing_func: CallableTypeVar) -> CallableTypeVar:
         # tuple & str forms of input args
         input_arg_dict_items = args_dict.items()
         input_arg_tuple = tuple(input_arg_dict_items)
-        input_arg_strs = [f'{k}={v}' for k, v in input_arg_dict_items]
+        input_arg_strs = [f'{k}={stringify_device_or_enum(v)}'
+                          for k, v in input_arg_dict_items]
 
         if set is None:
+            return_annotation = sensing_func.__annotations__.get('return')
+            return_annotation_str = (f': {return_annotation}'
+                                     if return_annotation
+                                     else '')
             print_str = (f'SENSE: {self}.{sensing_func_name}'
-                         f"({', '.join(input_arg_strs)}) = ")
+                         f"({', '.join(input_arg_strs)})"
+                         f'{return_annotation_str} = ')
 
             # if input_arg_tuple is in current sensing states,
             # then pop and return corresponding value
@@ -92,16 +97,11 @@ def sense(sensing_func: CallableTypeVar) -> CallableTypeVar:
 
             # else ask user for direct input
             else:
-                return_annotation = sensing_func.__annotations__.get('return')
-                return json.loads(input(f'{print_str}?' +
-                                        (f' [{return_annotation}]'
-                                         if return_annotation
-                                         else '') +
-                                        ' (in JSON)   '))
+                return json.loads(input(f'{print_str}? (in JSON)   '))
 
         else:
             # set the provided value in current sensing states
-            sensing_state_dict[input_args_tuple] = set
+            sensing_state_dict[input_arg_tuple] = set
             print(f'SET: {self}.{sensing_state_dict_name}'
                   f"[{', '.join(input_arg_strs)}] = {set}")
 
