08842c8adb81e78cc1dedab3d3b2b530485ef6d8
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-08-12 22:48:55 -0700

    update smartdrive module

--- src/smartdrive.py
+++ src/smartdrive.py
@@ -291,7 +291,7 @@ class Smartdrive(Drivetrain):
         - velocityUnits: unit for the velocity parameter,
                          a VelocityUnits enum value
         """
-        self.drive_velocities[velocityUnits]: float = velocity
+        self.drive_velocities[velocityUnits] = velocity
 
     @act
     def set_turn_velocity(
@@ -307,7 +307,7 @@ class Smartdrive(Drivetrain):
         - velocityUnits: unit for the velocity parameter,
                          a VelocityUnits enum value
         """
-        self.turn_velocities[velocityUnits]: float = velocity
+        self.turn_velocities[velocityUnits] = velocity
 
     @act
     def set_timeout(self, time: float, timeUnits: TimeUnits = TimeUnits.SEC):
@@ -321,7 +321,7 @@ class Smartdrive(Drivetrain):
         - time: the amount of time.
         - timeUnits: unit for the time parameter, a TimeUnits enum value
         """
-        self.timeouts[timeUnits]: float = time
+        self.timeouts[timeUnits] = time
 
     @sense
     def timeout(self, timeUnits: TimeUnits = TimeUnits.SEC) -> float:
