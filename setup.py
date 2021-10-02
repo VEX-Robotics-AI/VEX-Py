3b7fab2b01528e302bb622dfd3372c00228e37e1
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-01 23:42:43 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup
 
 
-setup(
-    packages=['vex'],
-    py_modules=['drivetrain', 'motor_group', 'smartdrive', 'timer', 'vision',
-                'vexcode', '__decor'],
-    package_dir={'': 'src'},
-    include_package_data=True,
-    zip_safe=False,
-    namespace_packages=[]
-)
+setup(packages=['vex'],
+      py_modules=['drivetrain', 'motor_group', 'smartdrive', 'timer', 'vision',
+                  'vexcode', '__decor'],
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      namespace_packages=[])
