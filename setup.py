b8013ef48349de773c73ec90ba3556a971f71e9c
Vinh Luong <MBALearnsToCode@UChicago.edu>
2022-03-18 14:05:29 -0700

    refactor setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+"""Setup script."""
+
+
 from setuptools import setup
 
 
 setup(name='RobotMesh-VEXIQ-PyB-API-Stubs',
-      version='1.0.10',
+      version='1.0.7',
       packages=['vex'],
       py_modules=['drivetrain', 'motor_group', 'smartdrive', 'timer', 'vision',
                   'vexcode', '__decor'],
