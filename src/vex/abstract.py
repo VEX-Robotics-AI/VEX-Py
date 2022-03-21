a19bd21835714db15e40ccc56db8872a72f5c9f8
Vinh Luong <MBALearnsToCode@UChicago.edu>
2022-03-20 21:29:44 -0700

    refactor vex.abstract

--- src/vex/abstract.py
+++ src/vex/abstract.py
@@ -1,12 +1,20 @@
+"""VEX Abstract Base Classes."""
+
+
+from collections.abc import Sequence
+
 from .port import Ports
 
 
+__all__: Sequence[str] = 'Device', 'SingletonDevice'
+
+
 class Device:
-    """
-    Base class for all Vex IQ devices.
-    """
+    """Base class for all Vex IQ devices."""
+
     @property
     def port(self) -> Ports:
+        """Port."""
         return self._port
 
     @port.setter
@@ -14,9 +22,13 @@ class Device:
         self._port = port
 
     def __str__(self) -> str:
+        """Return string representation."""
         return f'{type(self).__name__}({self.port.name})'
 
 
-class SingletonDevice:
+class SingletonDevice:   # pylint: disable=too-few-public-methods
+    """Singleton Device."""
+
     def __str__(self) -> str:
+        """Return string representation."""
         return type(self).__name__
