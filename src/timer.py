54795d469ab00936aa826787310584c09e90af5e
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-03-20 23:46:02 -0700

    refactor timer module

--- src/timer.py
+++ src/timer.py
@@ -1,74 +1,54 @@
-"""
-Timer module.
-"""
+"""Timer."""
 
+
+from collections.abc import Sequence
 
 from __vex_decor import act, sense
 
 
+__all__: Sequence[str] = ('Timer',)
+
+
 class Timer:
     def __init__(self):
-        """
-        Create the Timer class, does not start the timer.
-        """
+        """Create the Timer class, does not start the timer."""
 
     @act
     def reset(self):
-        """
-        Reset the time, clearing all data.
-        """
+        """Reset the time, clearing all data."""
 
     @act
     def start(self):
-        """
-        Start or resume timing the current lap.
-        """
+        """Start or resume timing the current lap."""
 
     @act
     def stop(self):
-        """
-        Stop or pause timing the current lap.
-        """
+        """Stop or pause timing the current lap."""
 
     @act
     def start_lap(self):
-        """
-        Stops the current lap and starts a new one.
-        """
+        """Stop the current lap and starts a new one."""
 
     @sense
     def is_running(self) -> bool:
-        """
-        True if timer is currently running.
-        """
+        """Return True if timer is currently running."""
 
     @sense
     def elapsed_time(self, lap_index: int = -1) -> float:
         """
-        Elapsed time for the current lap (no args)
-        or the lap with the given index.
+        Return elapsed time for current lap (no args) or lap with given index.
 
         Negative indices (from the end) supported. Time in seconds.
         """
 
     @sense
     def lap_count(self) -> int:
-        """
-        Number of laps stored (including the current one).
-        """
+        """Return number of laps stored (including the current one)."""
 
     @sense
     def total_time(self) -> float:
-        """
-        Total elapsed time for all laps.
-
-        Time in seconds.
-        """
+        """Return total elapsed time for all laps (time in seconds)."""
 
     @sense
     def average_time(self) -> float:
-        """
-        Average time per lap.
-
-        Time in seconds.
-        """
+        """Return average time per lap (time in seconds)."""
