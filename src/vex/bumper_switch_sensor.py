b5530371f6e61ec8ee54baafd87e16d6abe5a9b4
Vinh Luong <MBALearnsToCode@UChicago.edu>
2022-03-20 21:50:14 -0700

    refactor vex.bumper_switch_sensor

--- src/vex/bumper_switch_sensor.py
+++ src/vex/bumper_switch_sensor.py
@@ -1,13 +1,23 @@
+"""VEX Bumper Switch Sensor."""
+
+
+from collections.abc import Sequence
+
 from __vex_decor import sense
 
 from .abstract import Device
 from .port import Ports
 
 
+__all__: Sequence[str] = ('Bumper',)
+
+
 class Bumper(Device):
+    """Bumper Switch Sensor."""
+
     def __init__(self, index: Ports):
         """
-        Creates a new bumper object on the port specified in the parameter.
+        Create a new bumper object on the port specified in the parameter.
 
         param:
         index: The port index for this bumper. The index is zero-based.
