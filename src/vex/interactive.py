5467df6c41e96102d63c1d52d6e459ce042ea804
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-03-20 22:22:34 -0700

    refactor vex.interactive

--- src/vex/interactive.py
+++ src/vex/interactive.py
@@ -1,4 +1,14 @@
-# INTERACTIVE MODE FLAG
-# in this mode,
-# sensing functions ask for user inputs when there are no existing set states
-ON = True
+"""Interactive Mode Flag.
+
+In this mode, sensing functions ask for user inputs
+when there are no existing set states.
+"""
+
+
+from collections.abc import Sequence
+
+
+__all__: Sequence[str] = ('ON',)
+
+
+ON: bool = True
