2d55c16c1e740ad9982cf3685e50def3075410b4
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-03-20 22:58:27 -0700

    refactor vex.time

--- src/vex/time.py
+++ src/vex/time.py
@@ -1,20 +1,28 @@
+"""Time Measurements."""
+
+
+from collections.abc import Sequence
+from enum import IntEnum
+
 from __vex_decor import act
 
-from enum import IntEnum
+
+__all__: Sequence[str] = 'TimeUnits', 'wait'
 
 
 class TimeUnits(IntEnum):
-    """
-    The measurement units for time values.
-    """
+    """The measurement units for time values."""
+
     SEC: int = 0   # A time unit that is measured in seconds.
     MSEC: int = 1   # A time unit that is measured in milliseconds.
 
 
+# pylint: disable=unused-argument
 @act
 def wait(time: float, timeUnits: TimeUnits = TimeUnits.SEC):
     """
     Wait for a specific amount of time.
+
     Identical to sys.sleep().
 
     Parameters
