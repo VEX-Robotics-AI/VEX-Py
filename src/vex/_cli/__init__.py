5a1b6b3bb56af1fe7db327c4b41dfd94a1bfa12e
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-03-23 22:55:30 -0700

    add vex.cli.robotmesh_vex_cli

---
+++ src/vex/_cli/__init__.py
@@ -0,0 +1,31 @@
+"""Robot Mesh VEX CLI."""
+
+
+import click
+
+
+@click.group(name='h1st',
+             cls=click.Group,
+             commands={
+             },
+
+             # Command kwargs
+             context_settings=None,
+             # callback=None,
+             # params=None,
+             help='Robot Mesh VEX CLI >>>',
+             epilog='^^^ Robot Mesh VEX CLI',
+             short_help='Robot Mesh VEX CLI',
+             options_metavar='[OPTIONS]',
+             add_help_option=True,
+             no_args_is_help=True,
+             hidden=False,
+             deprecated=False,
+
+             # Group/MultiCommand kwargs
+             invoke_without_command=False,
+             subcommand_metavar='ROBOTMESH_VEX_SUB_COMMAND',
+             chain=False,
+             result_callback=None)
+def robotmesh_vex_cli():
+    """Robot Mesh VEX CLI."""
