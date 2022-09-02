01ec111cc57e254be790c2599485748cab672702
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-09-01 21:37:05 -0700

    update src.vex.bumper_switch_sensor.Bumper doc

--- src/vex/bumper_switch_sensor.py
+++ src/vex/bumper_switch_sensor.py
@@ -8,6 +8,9 @@ from abm.decor import sense
 from ._abstract import Device
 from .port import Ports
 
+# pylint: disable=unused-import
+from .util.doc import robotmesh_doc, vexcode_doc   # noqa: F401
+
 
 __all__: Sequence[str] = ('Bumper',)
 
@@ -15,24 +18,26 @@ __all__: Sequence[str] = ('Bumper',)
 class Bumper(Device):
     """Bumper Switch Sensor."""
 
-    def __init__(self, index: Ports):
-        """
+    @robotmesh_doc("""
         Create a new bumper object on the port specified in the parameter.
 
         param:
         index: The port index for this bumper. The index is zero-based.
-        """
+    """)
+    def __init__(self, index: Ports):
+        """Initialize Bumper Switch Sensor."""
         self.port: Ports = index
 
     def __hash__(self) -> int:
         """Return Integer Hash."""
         raise hash(self.port)
 
-    @sense
-    def pressing(self) -> bool:
-        """
+    @robotmesh_doc("""
         Get the pressed status of the bumper device.
 
         Returns
         True if pressed, False otherwise.
-        """
+    """)
+    @sense
+    def pressing(self) -> bool:
+        """Return Bumper's pressed status."""
