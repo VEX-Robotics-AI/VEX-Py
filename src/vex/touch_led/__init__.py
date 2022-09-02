f60147d9bd4c0dcbc30f7704c802e083db4260de
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-09-02 09:35:51 -0700

    update src.vex.touch_led docs

--- src/vex/touch_led/__init__.py
+++ src/vex/touch_led/__init__.py
@@ -2,77 +2,84 @@
 
 
 from collections.abc import Sequence
-from enum import IntEnum
 
 from abm.decor import act, sense
 
-from ._abstract import Device
-from .color_sensor import ColorHue
-from .port import Ports
+from .._abstract import Device
+from ..color_sensor import ColorHue
+from ..port import Ports
+
+# pylint: disable=unused-import
+from ..util.doc import robotmesh_doc, vexcode_doc   # noqa: F401
+
+from .fade_type import FadeType
 
 
 __all__: Sequence[str] = 'Touchled', 'FadeType'
 
 
-class FadeType(IntEnum):
-    """Fade types."""
+@robotmesh_doc("""
+    Use this class when programming with the touch LED device.
 
-    OFF: int = 0
-    SLOW: int = 1
-    FAST: int = 2
-
-
+    robotmesh.com/studio/content/docs/vexiq-python_b/html/classvex_1_1_touchled.html
+""")
 class Touchled(Device):
-    """Use this class when programming with the touch LED device."""
+    """Touch LED."""
 
     def __init__(self, index: Ports):
         """Initialize Touch LED."""
         self.port: Ports = index
 
-    @act
-    def default_fade(self, fadeType: FadeType):
-        """
+    def __hash__(self) -> int:
+        """Return Integer Hash."""
+        raise hash(self.port)
+
+    @robotmesh_doc("""
         Set the default fade time for the touchled sensor.
 
         Parameters:
         - fadeType: The type of fade the touchled
                     will use: FadeType.SLOW, FAST or OFF
-        """
+    """)
+    @act
+    def default_fade(self, fadeType: FadeType):
+        """Set default Fade Type."""
         # pylint: disable=attribute-defined-outside-init
         self.fade_type: FadeType = fadeType
 
-    @sense
-    def pressing(self) -> bool:
-        """
+    @robotmesh_doc("""
         Get the pressed status of the touchled device.
 
         Returns:
         True if pressed, False otherwise
-        """
+    """)
+    @sense
+    def pressing(self) -> bool:
+        """Return Touch LED's pressed status."""
 
-    @act
-    def on(self, color: hex, brightness: int = 100):
-        """
+    @robotmesh_doc("""
         Turn on the led in the touchled sensor.
 
         Parameters:
         - color: color value: 0xRRGGBB
         - brightness: The brightness for the led
-        """
-
+    """)
     @act
-    def on_hue(self, colorHue: ColorHue, brightness: int = 100):
-        """
+    def on(self, color: hex, brightness: int = 100):
+        """Turn on color."""
+
+    @robotmesh_doc("""
         Turn on the led in the touchled sensor.
 
         Parameters:
         - colorHue: The color of the led: ColorHue enum value
         - brightness: The brightness for the led
-        """
-
+    """)
     @act
-    def on_rgb(self, red: int, green: int, blue: int, brightness: int = 100):
-        """
+    def on_hue(self, colorHue: ColorHue, brightness: int = 100):
+        """Turn on Color Hue."""
+
+    @robotmesh_doc("""
         Turn on the led in the touchled sensor.
 
         Parameters:
@@ -80,50 +87,55 @@ class Touchled(Device):
         - green: The green value of the led, 0-255
         - blue: The blue value of the led, 0-255
         - brightness: The brightness for the led, 0-100
-        """
+    """)
+    @act
+    def on_rgb(self, red: int, green: int, blue: int, brightness: int = 100):
+        """Turn on RGB color."""
 
+    @robotmesh_doc("""
+        Turn off the led in the touchled sensor.
+    """)
     @act
     def off(self):
-        """Turn off the led in the touchled sensor."""
+        """Turn off LED."""
 
-    @act
-    def brightness(self, brightness: int):
-        """
+    @robotmesh_doc("""
         Turn on the led in the touchled sensor, or change current brightness.
 
         Parameters:
         - brightness: The brightness for the led 0-100
-        """
-
+    """)
     @act
-    def blink(self, color: hex, on_time: float = 0.25, off_time: float = 0.25):
-        """
+    def brightness(self, brightness: int):
+        """Set Brightness Percent Level."""
+
+    @robotmesh_doc("""
         Set the led in the touchled sensor as blinking.
 
         Parameters:
         - color: color value: 0xRRGGBB
         - on_time: The time the led should remain on in seconds
         - off_time: The time the led should remain off in seconds
-        """
-
+    """)
     @act
-    def blink_hue(
-            self, colorHue: ColorHue,
-            on_time: float = 0.25, off_time: float = 0.25):
-        """
+    def blink(self, color: hex, on_time: float = 0.25, off_time: float = 0.25):
+        """Blink color."""
+
+    @robotmesh_doc("""
         Set the led in the touchled sensor as blinking.
 
         Parameters:
         - colorHue: The color of the led: ColorHue enum value
         - on_time: The time the led should remain on in seconds
         - off_time: The time the led should remain off in seconds
-        """
-
+    """)
     @act
-    def blink_rgb(   # pylint: disable=too-many-arguments
-            self, red: int, green: int, blue: int,
+    def blink_hue(
+            self, colorHue: ColorHue,
             on_time: float = 0.25, off_time: float = 0.25):
-        """
+        """Blink Color Hue."""
+
+    @robotmesh_doc("""
         Set the led in the touchled sensor as blinking.
 
         Parameters:
@@ -132,4 +144,9 @@ class Touchled(Device):
         - blue: The blue value of the led 0-255
         - on_time: The time the led should remain on in seconds
         - off_time: The time the led should remain off in seconds
-        """
+    """)
+    @act
+    def blink_rgb(   # pylint: disable=too-many-arguments
+            self, red: int, green: int, blue: int,
+            on_time: float = 0.25, off_time: float = 0.25):
+        """Blink RGB Color."""
---
+++ src/vex/touch_led/fade_type.py
@@ -0,0 +1,21 @@
+"""Touch LED Fade Types."""
+
+
+from collections.abc import Sequence
+from enum import IntEnum
+
+from ..util.doc import robotmesh_doc
+
+
+__all__: Sequence[str] = ('FadeType',)
+
+
+@robotmesh_doc("""
+    robotmesh.com/studio/content/docs/vexiq-python_b/html/classvex_1_1_fade_type.html
+""")
+class FadeType(IntEnum):
+    """Touch LED Fade Types."""
+
+    OFF: int = 0
+    SLOW: int = 1
+    FAST: int = 2
