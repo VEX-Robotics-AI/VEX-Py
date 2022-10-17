43e432f98bcd465516aedc7de426d276339959b1
Bao Nguyen <nlbao95@gmail.com>
2022-10-16 22:13:42 -0700

    Adds brain.screen.set_cursor().

--- src/tests/brain/screen_test.py
+++ src/tests/brain/screen_test.py
@@ -4,13 +4,18 @@ from ...vex import Brain
 
 
 class TestBrainScreen(unittest.TestCase):
-
     def setUp(self):
         self.brain = Brain()
 
     def test_print_line(self):
-        self.brain.screen.print_line(2, 'Hello World')
+        self.brain.screen.print_line(2, "Hello World")
+
+    def test_screen_clear_screen(self):
+        self.brain.screen.clear_screen()
+
+    def test_screen_set_cursor(self):
+        self.brain.screen.set_cursor(1, 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- src/vex/brain/screen.py
+++ src/vex/brain/screen.py
@@ -9,21 +9,24 @@ from .._abstract_device import SingletonDevice
 from ..util.doc import robotmesh_doc, vexcode_doc
 
 
-__all__: Sequence[str] = ('BrainLcd',)
+__all__: Sequence[str] = ("BrainLcd",)
 
 
-@robotmesh_doc("""
+@robotmesh_doc(
+    """
     Use this class to write or draw to the brain's LCD screen.
 
     * 21 characters wide
     * 5 lines (1-5)
 
     robotmesh.com/studio/content/docs/vexiq-python_b/html/classvex_1_1_brain_lcd.html
-""")
+"""
+)
 class BrainLcd(SingletonDevice):
     """Brain LCD Screen."""
 
-    @robotmesh_doc("""
+    @robotmesh_doc(
+        """
         Print a number, string, or boolean at a particular line.
 
         (clearing the rest of the line)
@@ -35,22 +38,38 @@ class BrainLcd(SingletonDevice):
               For multiple arguments, use format like
               "x: %g y: %g" % (x, y) -> "x: 123 y: 456"
               Supported format flags are g (all) x (hex) d (int) f (float)
-    """)
+    """
+    )
     @act
     def print_line(self, number: int, text: str, /):
         """Print to a line on Brain LCD Screen."""
 
-    @robotmesh_doc("""
+    @robotmesh_doc(
+        """
         Clear the whole screen.
-    """)
-    @vexcode_doc("""
+    """
+    )
+    @vexcode_doc(
+        """
         Clears the entire VEX IQ Brain's Screen.
 
         Clear Screen will not reset the Brain's screen cursor.
 
         Use the Set Cursor command to set the Brain's cursor
         to the desired position.
-    """)
+    """
+    )
     @act
     def clear_screen(self):
         """Clear Brain LCD Screen."""
+
+    @vexcode_doc(
+        """
+        Set the cursor location for Brain's Print commands.
+        
+        brain.screen.set_cursor(ROW, COL).
+    """
+    )
+    @act
+    def set_cursor(self, row, col):
+        """Set the cursor location for Brain's Print commands."""
