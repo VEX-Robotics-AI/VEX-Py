93dc361b2ab86d95c13810b8b230fe76dae3425a
Bao Nguyen <nlbao95@gmail.com>
2022-10-23 18:43:47 -0700

    Adds Sonar.object_distance(MM).

---
+++ src/tests/distance_sensor/distance_sensor_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from ...vex import Sonar, Ports, MM
+from ...testing.io_utils import replace_stdin
+
+
+class TestSonar(unittest.TestCase):
+    def setUp(self):
+        self.distance_sensor = Sonar(Ports.PORT1)
+
+    def test_object_distance(self):
+        with replace_stdin("""123.4"""):
+            assert self.distance_sensor.object_distance(MM) == 123.4
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/vex/distance_sensor/__init__.py
+++ src/vex/distance_sensor/__init__.py
@@ -10,49 +10,56 @@ from ..brain.port import Ports
 from ..units_common import DistanceUnits
 
 # pylint: disable=unused-import
-from ..util.doc import robotmesh_doc, vexcode_doc   # noqa: F401
+from ..util.doc import robotmesh_doc, vexcode_doc  # noqa: F401
 
 
-__all__: Sequence[str] = ('Sonar',)
+__all__: Sequence[str] = ("Sonar",)
 
 
-@robotmesh_doc("""
+@robotmesh_doc(
+    """
     robotmesh.com/studio/content/docs/vexiq-python_b/html/classvex_1_1_sonar.html
-""")
+"""
+)
 class Sonar(Device):
     """Sonar (Distance Sensor)."""
 
-    @robotmesh_doc("""
+    @robotmesh_doc(
+        """
         Create new sonar sensor object on the port specified in the parameter.
 
         Parameters:
         - index: to the brain port.
-    """)
+    """
+    )
     def __init__(self, index: Ports, /):
         """Initialize Distance Sensor."""
         self.port: Ports = index
 
-        self.max_distances: dict[DistanceUnits, float] = \
-            dict[DistanceUnits, float]()
+        self.max_distances: dict[DistanceUnits, float] = dict[DistanceUnits, float]()
 
     def __hash__(self) -> int:
         """Return Integer Hash."""
         raise hash(self.port)
 
-    @robotmesh_doc("""
+    @robotmesh_doc(
+        """
         Set the maximum distance (default 2.5m).
 
         Parameters:
         - distance: maximum distance to be measured in units
         - distanceUnits: a DistanceUnits enum value for the measurement unit.
-    """)
+    """
+    )
     @act
-    def set_maximum(self, distance: float,
-                    distanceUnits: DistanceUnits = DistanceUnits.MM, /):
+    def set_maximum(
+        self, distance: float, distanceUnits: DistanceUnits = DistanceUnits.MM, /
+    ):
         """Set maximum measurable distance."""
         self.max_distances[distanceUnits] = distance
 
-    @robotmesh_doc("""
+    @robotmesh_doc(
+        """
         Get the value of the sonar sensor.
 
         Parameters:
@@ -61,7 +68,28 @@ class Sonar(Device):
 
         Returns:
         an integer that represents the unit value specified by the parameter.
-    """)
+    """
+    )
     @sense
-    def distance(self, distanceUnits: DistanceUnits = DistanceUnits.MM, /) -> int:   # noqa: E501
+    def distance(
+        self, distanceUnits: DistanceUnits = DistanceUnits.MM, /
+    ) -> int:  # noqa: E501
+        """Return measured distance to nearby object."""
+
+    @vexcode_doc(
+        """
+        Get the value of the sonar sensor.
+
+        Parameters:
+        - distanceUnits: The measurement unit for
+                         the sonar device, DistanceUnits enum value.
+
+        Returns:
+        an integer that represents the unit value specified by the parameter.
+    """
+    )
+    @sense
+    def object_distance(
+        self, distanceUnits: DistanceUnits = DistanceUnits.MM, /
+    ) -> int:  # noqa: E501
         """Return measured distance to nearby object."""
