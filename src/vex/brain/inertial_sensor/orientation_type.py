ab8b5d20e719a3ff88aa300da2aa02de97036a46
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-11 17:29:54 -0800

    update vex.brain.inertial_sensor.orientation_type submodule

--- src/vex/brain/inertial_sensor/orientation_type.py
+++ src/vex/brain/inertial_sensor/orientation_type.py
@@ -2,26 +2,25 @@
 
 
 from collections.abc import Sequence
-from enum import IntEnum
+from enum import IntEnum, auto
 
 from ...util.doc import vexcode_doc
 
 
-__all__: Sequence[str] = ("OrientationType",)
+__all__: Sequence[str] = ('OrientationType',)
 
 
-@vexcode_doc(
-    """VEX IQ (2nd generation) OrientationType types.
-    """
-)
+@vexcode_doc("""
+    VEX IQ (2nd generation) Orientation Types.
+""")
 class OrientationType(IntEnum):
-    """VEX IQ (2nd generation) OrientationType types."""
+    """VEX IQ (2nd generation) Orientation Types."""
 
-    # Pitch is the rotation around the side to side axis,
-    PITCH: int = 0
+    # Pitch is the rotation around the side-to-side axis
+    PITCH: int = auto()
 
-    # Roll is the rotation around the front to back axis.
-    ROLL: int = 1
+    # Roll is the rotation around the front-to-back axis
+    ROLL: int = auto()
 
-    # Yaw is rotation around the vertical axis.
-    YAW: int = 2
+    # Yaw is rotation around the vertical axis
+    YAW: int = auto()
