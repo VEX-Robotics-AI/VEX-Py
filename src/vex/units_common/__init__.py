7a204a47ea06bc8b60552778bb576bd43737c33c
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-13 14:53:45 -0800

    rename vex.units_common submodules

--- src/vex/units_common/__init__.py
+++ src/vex/units_common/__init__.py
@@ -3,8 +3,8 @@
 
 from collections.abc import Sequence
 
-from .distance_units import DistanceUnits, MM, INCHES, CM
-from .rotation_units import RotationUnits, DEGREES, TURNS
+from .distance import DistanceUnits, MM, INCHES, CM
+from .rotation import RotationUnits, DEGREES, TURNS
 
 
 __all__: Sequence[str] = ('DistanceUnits', 'MM', 'INCHES', 'CM',
