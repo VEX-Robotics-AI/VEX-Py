74164420e0712fe8184a530a0ddba21f2997410f
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-13 14:40:41 -0800

    update vex.optical_sensor.led_state_type submodule

--- src/vex/optical_sensor/led_state_type.py
+++ src/vex/optical_sensor/led_state_type.py
@@ -1,19 +1,15 @@
-"""VEX Optical Sensor LED State Type."""
+"""Optical Sensor LED State Type."""
+
 
 from collections.abc import Sequence
 from enum import IntEnum, auto
 
-from ..util.doc import vexcode_doc
+
+__all__: Sequence[str] = ('LedStateType',)
 
 
-__all__: Sequence[str] = 'LedStateType'
-
-
-@vexcode_doc("""
-    VEX Optical Sensor LED State Type.
-""")
 class LedStateType(IntEnum):
-    """VEX Optical Sensor LED State Type."""
+    """Optical Sensor LED State Type."""
 
     ON: int = auto()
     OFF: int = auto()
