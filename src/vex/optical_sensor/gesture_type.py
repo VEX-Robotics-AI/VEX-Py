9a6c047607962972eeea8ad584f8b89db0724a14
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-13 14:37:35 -0800

    update vex.optical_sensor.gesture_type submodule

--- src/vex/optical_sensor/gesture_type.py
+++ src/vex/optical_sensor/gesture_type.py
@@ -1,19 +1,15 @@
-"""Gesture Types."""
+"""Optical Sensor Gesture Types."""
+
 
 from collections.abc import Sequence
 from enum import IntEnum, auto
 
-from ..util.doc import vexcode_doc
+
+__all__: Sequence[str] = ('GestureType',)
 
 
-__all__: Sequence[str] = 'GestureType'
-
-
-@vexcode_doc("""
-    VEX Optical Sensor Gesture Type.
-""")
 class GestureType(IntEnum):
-    """VEX Optical Sensor Gesture Type."""
+    """Optical Sensor Gesture Types."""
 
     UP: int = auto()
     DOWN: int = auto()
