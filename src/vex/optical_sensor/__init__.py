737d970a487e69245d60d95be6a4819f5969ef51
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-13 15:29:27 -0800

    update vex.optical_sensor.Optical.gesture_disable() method

--- src/vex/optical_sensor/__init__.py
+++ src/vex/optical_sensor/__init__.py
@@ -68,33 +68,18 @@ class OpticalSensor(Device):
         """Enable gesture detection."""
 
     @vexcode_doc("""
-        The Optical Gesture Disable command allows you to set an IQ Optical Sensor to detect colors.
+        Optical Gesture Disable
+
+        Sets an IQ Optical Sensor to detect colors.
+
+        The Optical Gesture Disable command allows you
+        to set an IQ Optical Sensor to detect colors.
 
         By default, an IQ Optical Sensor will be set to detect colors.
-
-        The example shows how to correctly set an IQ Optical Sensor to detect colors after checking for gestures.
-            # Detect gestures
-            optical.gesture_enable()
-            while optical.get_gesture().type != GestureType.UP:
-                wait(0.1, SECONDS)
-            brain.screen.print("Up!")
-
-            wait(1, SECONDS)
-
-            # Detect colors
-            optical.gesture_disable()
-            while True:
-                brain.screen.clear_screen()
-                brain.screen.set_cursor(1, 1)
-                if optical.color() == Color.RED:
-                    brain.screen.print("Red Object!")
-                else:
-                    brain.screen.print("Not Red Object!")
-                wait(0.02, SECONDS)
     """)
     @act
     def gesture_disable(self):
-        """Disables gesture."""
+        """Disable gesture detection."""
 
     @vexcode_doc("""
         The Optical Gesture Detected command returns an object with gesture information.
