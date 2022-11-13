ea4b5588f848e03a649d9707eadae9cd763e6e3a
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-13 15:00:31 -0800

    WiP edits: vex.optical_sensor submodule

--- src/vex/optical_sensor/__init__.py
+++ src/vex/optical_sensor/__init__.py
@@ -1,24 +1,24 @@
 
-"""VEX Optical Sensor."""
+"""Optical Sensor."""
+
 
 from __future__ import annotations
 
 from collections.abc import Sequence
-
-from abm.decor import act, sense
 from typing import Callable
 
-from vex import PERCENT, ColorHue, VelocityUnits
-from .._abstract_device import SingletonDevice
+from abm.decor import act, sense
+
+from .._abstract_device import Device
+from ..brain.port import Ports
+from ..color_sensor.color_hue import ColorHue
+from ..motor.velocity_units import VelocityUnits, PERCENT
 from ..util.doc import vexcode_doc
 from .led_state_type import LedStateType
 from .gesture_type import GestureType
 
-__all__: Sequence[str] = (
-    "OpticalSensor",
-    "LedStateType",
-    "GestureType",
-)
+
+__all__: Sequence[str] = 'OpticalSensor', 'GestureType', 'LedStateType'
 
 
 _GESTURE_CALLBACK_DOCSTRINGS = """
@@ -39,19 +39,18 @@ _GESTURE_CALLBACK_DOCSTRINGS = """
 """
 
 
-@vexcode_doc(
-    """
-    VEX Optical Sensor.
-    """
-)
-class OpticalSensor(SingletonDevice):
-    """VEX Optical Sensor."""
+class OpticalSensor(Device):
+    """Optical Sensor."""
 
-    def __init__(self):
-        pass
+    def __init__(self, port: Ports, /):
+        """Initialize Optical Sensor."""
+        self.port: Ports = port
 
-    @vexcode_doc(
-        """
+    def __hash__(self) -> int:
+        """Return Integer Hash."""
+        raise hash(self.port)
+
+    @vexcode_doc("""
         The Optical Gesture Enable command allows you to set an IQ Optical Sensor to detect gestures.
 
         By default, an IQ Optical Sensor will be set to detect colors.
@@ -62,14 +61,12 @@ class OpticalSensor(SingletonDevice):
             while optical.get_gesture().type != GestureType.UP:
                 wait(0.1, SECONDS)
             brain.screen.print("Up!")
-        """
-    )
+    """)
     @act
     def gesture_enable(self):
         """Enables gesture."""
 
-    @vexcode_doc(
-        """
+    @vexcode_doc("""
         The Optical Gesture Disable command allows you to set an IQ Optical Sensor to detect colors.
 
         By default, an IQ Optical Sensor will be set to detect colors.
@@ -93,18 +90,16 @@ class OpticalSensor(SingletonDevice):
                 else:
                     brain.screen.print("Not Red Object!")
                 wait(0.02, SECONDS)
-        """
-    )
+    """)
     @act
     def gesture_disable(self):
         """Disables gesture."""
 
-    @vexcode_doc(
-        """
+    @vexcode_doc("""
         The Optical Gesture Detected command returns an object with gesture information.
         To check the type of the detected gesture, access the type property on the returned object.
             optical.get_gesture().type
-        
+
         Before using any IQ Optical Sensor gesture commands, an IQ Optical Sensor must be
         correctly set to detect gestures with the optical.gesture_enable() command.
         Below are the gestures that an IQ Optical Sensor can detect:
@@ -118,14 +113,12 @@ class OpticalSensor(SingletonDevice):
             while optical.get_gesture().type != GestureType.UP:
                 wait(0.1, SECONDS)
             brain.screen.print("Up!")
-        """
-    )
+    """)
     @sense
     def get_gesture(self) -> GestureType:
         """Returns an object with gesture information."""
 
-    @vexcode_doc(
-        """
+    @vexcode_doc("""
         Set Optical Light allows you to turn the Optical Sensor's light on or off.
         The light lets the sensor see objects if it is looking at an object in a dark area.
 
@@ -133,40 +126,34 @@ class OpticalSensor(SingletonDevice):
             optical.set_light(LedStateType.ON)
         To turn the light off you pass LedStateType.OFF as the STATE parameter.
             optical.set_light(LedStateType.OFF)
-        """
-    )
+    """)
     @act
     def set_light(self, state: LedStateType):
         """Sets light to state."""
 
-    @vexcode_doc(
-        """
+    @vexcode_doc("""
         Set Optical Light Power will change the brightness of the light on an Optical Sensor.
 
         The command accepts a range of 0 to 100 percent for the POWER parameter, and PERCENT as the unit.
         If the light is off, this command will turn the light on.
-        """
-    )
+    """)
     @act
     def set_light_power(self, power: float, units : VelocityUnits = PERCENT):
         """Set the light power"""
 
-    @vexcode_doc(
-        """
+    @vexcode_doc("""
         Optical Is Near Object returns True if an object is detected, and returns False otherwise.
 
         It should be used to check if an object is close to the IQ Optical Sensor so that
         the color readings from the Optical Sensor's Color command is accurate.
             if optical.is_near_object() and optical.color() == Color.Red:
                 brain.screen.print("Red object!")
-        """
-    )
+    """)
     @sense
     def is_near_object(self) -> bool:
         """Returns True if an object is detected, and returns False otherwise."""
 
-    @vexcode_doc(
-        """
+    @vexcode_doc("""
         The Optical Color command returns the closest color match base on the hue of a detected object.
 
         The following colors can be used as a comparison to the color detected by an IQ Optical Sensor:
@@ -177,25 +164,22 @@ class OpticalSensor(SingletonDevice):
             Color.ORANGE
             Color.PURPLE
             Color.CYAN
-        
+
         The example prints to a VEX IQ Brain's screen if a red object is detected by an Optical Sensor.
             if optical.color() == Color.RED:
                 brain.screen.print("Red!")
-        """
-    )
+    """)
     @sense
     def color(self) -> ColorHue:
         """Returns the closest color match base on the hue of a detected object."""
 
-    @vexcode_doc(
-        """
+    @vexcode_doc("""
         Optical Brightness reports a range of values from 0 to 100 percent.
 
         A large amount of light detected will report a high brightness value.
         A small amount of light detected will report a low brightness value.
             brain.screen.print("Brightness:", optical.brightness())
-        """
-    )
+    """)
     @sense
     def brightness(self) -> float:
         """Reports a range of brightness from 0 to 100 percent."""
@@ -253,7 +237,7 @@ class OpticalSensor(SingletonDevice):
     @vexcode_doc(
         f"""
         {_GESTURE_CALLBACK_DOCSTRINGS}
-        
+
         Example:
             # Function to run when the event occurs
             def run_on_gesture_detected():
@@ -274,7 +258,7 @@ class OpticalSensor(SingletonDevice):
     @vexcode_doc(
        f"""
         {_GESTURE_CALLBACK_DOCSTRINGS}
-        
+
         Example:
             # Function to run when the event occurs
             def run_on_gesture_detected():
