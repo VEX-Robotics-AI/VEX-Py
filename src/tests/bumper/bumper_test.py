5b726abe774f296e8ec7f2ef2967abdfd666e59a
thaonhoang <thaoh553@gmail.com>
2022-11-17 23:40:43 -0500

    Pressed and Released are added

--- src/tests/bumper/bumper_test.py
+++ src/tests/bumper/bumper_test.py
@@ -1,19 +1,23 @@
 import unittest
 
 #import both Bumper and bumper_switch_sensor does not work
-from ...vex import ColorHue
-#from ...vex import Bumper
+#from ...vex import ColorHue
+from ...vex import Bumper, Ports
 
 
 def callback_func():
-    print("Button pressed")
+    print("Button pressed/released")
 
    
 class testBumperSwitchSensor(unittest.TestCase):
     def setUp(self):
-        self.bumper = Bumper()
+        self.bumper = Bumper(Ports.PORT1)
+
     def test_bumper_is_pressed(self):
         self.bumper.pressed(callback_func)
+
+    def test_bumper_is_released(self):
+        self.bumper.released(callback_func)
 
 
 if __name__ == "__main__":
--- src/vex/bumper_switch_sensor/__init__.py
+++ src/vex/bumper_switch_sensor/__init__.py
@@ -70,3 +70,30 @@ class Bumper(Device):
     @act
     def pressed(self, callback):
         """Return the specified callback function when a bumper is pressed"""
+
+
+    @vexcode_doc("""
+    Bumper Released
+    Runs the specified function when a bumper is released.
+        bumper.released(callback)
+    
+    How To Use
+    Create a function to call when the Bumper is released. Provide the name of the function that should run when the event occurs as the callback parameter.
+
+    def bumper_released():
+        brain.screen.print("Bumper released")
+    
+    Then, the function is passed as the parameter of the Bumper Released function call.
+        bumper.released(bumper_released)
+    
+    Callback Functions
+    A callback function is a function passed into another function as an argument. The code inside the callback function will run whenever the event occurs.
+
+    def callback_function():
+        brain.screen.print("Released")
+
+    bumper.released(callback_function)
+    """)
+    @act 
+    def released(self, callback):
+        """Return the specified callback function when bumper is released"""
