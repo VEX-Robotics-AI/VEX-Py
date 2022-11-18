f57af3753d22b150cbf67932a0b1eee7bbd354cc
thaonhoang <thaoh553@gmail.com>
2022-11-18 03:30:32 -0500

    Update API Color Sensor

--- src/vex/color_sensor/__init__.py
+++ src/vex/color_sensor/__init__.py
@@ -118,6 +118,105 @@ class ColorSensor(Device):
     def led(self, state: bool, /):
         """Set LED state."""
 
+    @vexcode_doc(
+        """
+        Color Is Near Object
+        Reports if a VEX IQ Color Sensor detects an object or surface.
+
+            color.is_near_object()
+        
+        How To Use
+        Reports True when a Color Sensor detects an object or surface close to the front of the sensor.
+
+        Reports False when a Color Sensor does not detect an object or surface close to the front of the sensor.
+
+        Example
+        The example below prints whether an object or surface is being detected by a Color Sensor.
+
+            brain.screen.print("True" if color.is_near_object() else "False")
+        """
+    )
+    @sense
+    def is_near_object(self) -> bool:
+        """Return True when a Color Sensor detects and object close to the front of the sensor, false otherwise."""
+
+    @vexcode_doc(
+        """
+        Color
+        Reports the color currently being detected by a VEX IQ Color Sensor.
+
+            color.color()
+
+        How To Use
+        Compare the value returned by Color to one of the following colors to check if a specific color is being detected:
+
+            Color.RED
+            Color.RED_VIOLET
+            Color.VIOLET
+            Color.BLUE_VIOLET
+            Color.BLUE
+            Color.BLUE_GREEN
+            Color.GREEN
+            Color.YELLOW_GREEN
+            Color.YELLOW
+            Color.YELLOW_ORANGE
+            Color.ORANGE
+            Color.RED_ORANGE
+
+        Example
+        The example below prints "Red" to the IQ Brain's screen if a red object is currently being detected by a Color Sensor.
+
+        if color.color() == Color.RED:
+            brain.screen.print("Red")
+
+        """
+    )
+    @sense
+    def color(self):
+        """Return the color currently being detected."""
+
+    @vexcode_doc(
+        """
+        Color Brightness
+        Reports the amount of light detected by a VEX IQ Color Sensor.
+
+            color.brightness()
+
+        How To Use
+        Color Brightness reports a range of values from 0 to 100 percent.
+
+        A larger amount of light will report a higher value, while a smaller amount of light will report a lower value.
+
+        Example
+        The example below prints the amount of light detected by a Color Sensor.
+
+            brain.screen.print("Brightness:", color.brightness())
+        """
+    )
+    @sense
+    def brightness(self):
+        """Return the amount of light detected by the Color Sensor."""
+
+    @vexcode_doc(
+        """
+        Color Hue
+        Reports the hue of the color detected by a VEX IQ Color Sensor.
+
+            color.hue()
+
+        How To Use
+        Color Hue reports a range of values from 0 to 360 degrees, which represents the position of the color on the color wheel.
+
+        Example
+        The example prints the hue of the color detected by the Color Sensor.
+
+            brain.screen.print("Hue:", color.hue())
+        """
+    )
+    @sense
+    def hue(self):
+        """Return the hue of the color detected by a VEX IQ Color Sensor."""
+
 
 # alias
 Colorsensor = ColorSensor
