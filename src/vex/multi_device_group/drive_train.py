911f02910565ad143036e57d200f2f6649c52c6f
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-19 23:14:18 -0800

    add vex.multi_device_group.drive_train.DriveTrain.drive_for(...) method

--- src/vex/multi_device_group/drive_train.py
+++ src/vex/multi_device_group/drive_train.py
@@ -78,3 +78,42 @@ class DriveTrain(MotorGroup):
     def drive(self, directionType: DirectionType = FORWARD, /):
         """Drive in specified direction."""
 
+    @vexcode_doc("""
+        Drive For
+
+        Moves the Drivetrain for a given distance.
+        All Drivetrain motors run forward or in reverse at the velocity
+        set by the Drivetrain's Set Drive Velocity command.
+        After the Drivetrain has moved the specified distance
+        the Drivetrain will stop.
+
+        Set how far the Drivetrain will move by entering at least
+        three valid arguments inside of the parentheses, separated by commas.
+
+        - The first argument specifies the direction.
+          Valid inputs are either FORWARD or REVERSE in all capital letters.
+
+        - The second argument specifies the distance
+          that the IQ Robot should drive.
+
+        - The third argument specifies the unit of measurement
+          that should be used. Valid inputs are either INCHES or MM
+          (millimeters) in all-capital letters.
+
+        The DISTANCE parameter accepts numeric values.
+        Negative values will cause the IQ Robot to drive
+        in the opposite of the input DIRECTION.
+
+        The Drive For command is by default a blocking command.
+        It will prevent subsequent commands from executing
+        until the Drivetrain movement has completed.
+
+        You can set a fourth parameter to wait=False to allow
+        proceeding commands to run even before the Drivetrain is done moving.
+    """)
+    @act
+    def drive_for(self, direction: DirectionType,
+                  distance: float = 200, unit: DistanceUnits = MM,
+                  wait: bool = True, /):
+        """Drive for a distance."""
+
