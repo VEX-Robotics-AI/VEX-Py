b91b63c2df37575b741bf85e2652d825095bd73b
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-19 23:42:20 -0800

    add vex.multi_device_group.drive_train.DriveTrain.set_turn_velocity(...) method

--- src/vex/multi_device_group/drive_train.py
+++ src/vex/multi_device_group/drive_train.py
@@ -2,7 +2,7 @@
 
 
 from collections.abc import Sequence
-from typing import Literal, Optional
+from typing import Literal
 from typing_extensions import Self
 
 from abm.decor import act, sense
@@ -212,4 +212,34 @@ class DriveTrain(MotorGroup):
     def set_drive_velocity(self, velocity: int, unit: VelocityUnits = PERCENT, /):  # noqa: E501
         """Set driving velocity."""
         self.drive_velocities[unit] = velocity
+
+    @vexcode_doc("""
+        Set Turn Velocity
+
+        Sets the Drivetrain's turn velocity.
+
+        Set Turn Velocity will set the velocity of the Drivetrain when turning,
+        but will not cause the Drivetrain to move
+        when used without a Turn or Turn For command.
+
+        Set Turn Velocity accepts a range of values
+        depending on what units are passed as the second parameter.
+
+        If PERCENT is used as the second parameter,
+        VELOCITY accepts a range from -100 to 100.
+
+        Alternatively, Set Turn Velocity can also use RPM as a valid UNITS
+        parameter, accepting a range from -127 to 127 as the VELOCITY.
+
+        Setting a Drivetrain's turn velocity to a negative value
+        will cause the Drivetrain to turn in the opposite of the direction
+        passed into any following drivtrain.turn commands.
+
+        Setting a Drivetrain's turn velocity to 0
+        will prevent the Drivetrain from turning.
+    """)
+    @act
+    def set_turn_velocity(self, velocity: int, unit: VelocityUnits = PERCENT, /):  # noqa: E501
+        """Set turning velocity."""
+        self.turn_velocities[unit] = velocity
 
