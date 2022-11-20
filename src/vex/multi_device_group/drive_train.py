44076f6bdefaead736268726fb831f7c61c5fe28
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-19 23:37:57 -0800

    add vex.multi_device_group.drive_train.DriveTrain.set_drive_velocity(...) method

--- src/vex/multi_device_group/drive_train.py
+++ src/vex/multi_device_group/drive_train.py
@@ -11,7 +11,7 @@ from ..motor import Motor
 from ..motor.brake_type import BrakeType
 from ..motor.direction_type import DirectionType, FORWARD
 from ..motor.turn_type import TurnType, RIGHT
-from ..motor.velocity_units import VelocityUnits
+from ..motor.velocity_units import VelocityUnits, PERCENT
 from ..time.time_units import TimeUnits
 from ..units_common.distance import DistanceUnits, MM
 from ..units_common.electric import CurrentUnits
@@ -41,6 +41,10 @@ class DriveTrain(MotorGroup):
         self.length_unit: DistanceUnits = length_unit
         self.gear_ratio: float = gear_ratio
 
+        self.drive_velocities: dict[VelocityUnits, float] = \
+            dict[VelocityUnits, float]()
+        self.turn_velocities: dict[VelocityUnits, float] = \
+            dict[VelocityUnits, float]()
 
     def __eq__(self, other: Self) -> bool:
         """Check Equality."""
@@ -178,4 +182,34 @@ class DriveTrain(MotorGroup):
     @act
     def stop(self):
         """Stop motors."""
+
+    @vexcode_doc("""
+        Set Drive Velocity
+
+        Sets the velocity of the Drivetrain for Drive and Drive For commands.
+
+        Set Drive Velocity will set the Drivetrain's velocity
+        when used with a Drive or Drive For command.
+        It will not cause the Drivetrain to move when used by itself.
+
+        Set Drive Velocity accepts a range of values
+        depending on what units are passed as the second parameter.
+
+        If PERCENT is used as the second parameter,
+        VELOCITY accepts a range from -100 to 100.
+
+        Set Drive Velocity can also use RPM as a valid UNITS parameter,
+        accepting a range from -127 to 127.
+
+        Setting a Drivetrain's drive velocity to a negative value
+        will cause the Drivetrain to drive opposite of the DIRECTION
+        passed into a Drive or Drive For command.
+
+        Setting a Drivetrain's drive velocity to 0 will prevent the Drivetrain
+        from moving even if a Drive or Drive For command is used.
+    """)
+    @act
+    def set_drive_velocity(self, velocity: int, unit: VelocityUnits = PERCENT, /):  # noqa: E501
+        """Set driving velocity."""
+        self.drive_velocities[unit] = velocity
 
