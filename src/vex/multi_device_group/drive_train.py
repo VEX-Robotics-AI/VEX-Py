22b4efc24eb612841d3f88eca28b12fab3e349f3
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-20 16:05:51 -0800

    add vex.multi_device_group.drive_train.DriveTrain.is_moving() method

--- src/vex/multi_device_group/drive_train.py
+++ src/vex/multi_device_group/drive_train.py
@@ -280,3 +280,19 @@ class DriveTrain(MotorGroup):
         """Set Motor Timeout."""
         self.timeout: float = time
 
+    @vexcode_doc("""
+        Drive Is Moving
+
+        Reports if the Drivetrain is currently moving.
+
+        Drive Is Moving reports True when the Drivetrain's motors are moving.
+        Drive Is Moving reports False when the Drivetrain's motors are stopped.
+
+        Note: This command will always return false if the Drivetrain is moving
+        because of a drivetrain.drive or drivetrain.turn command
+        (which do not specify a set distance to drive).
+    """)
+    @sense
+    def is_moving(self) -> bool:
+        """Report if the Drivetrain is currently moving."""
+
