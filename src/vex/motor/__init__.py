374340934fa6402b0ab8d7b8360d2c5eaed4bdec
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-20 23:41:13 -0800

    add vex.motor.Motor.set_position(...) method

--- src/vex/motor/__init__.py
+++ src/vex/motor/__init__.py
@@ -136,6 +136,24 @@ class Motor(Device):
     def stop(self, brakeType: Optional[BrakeType] = None, /):
         """Stop."""
 
+    @vexcode_doc("""
+        Set Motor Position
+
+        Sets an IQ Motor's or Motor Group's encoder(s) position
+        to the given position value.
+
+        This command can be used to set a Motor or Motor Group's position
+        to a given positional value.
+
+        Usually, the Set Motor Position command is used to set the position to
+        0 to reset a Motor or Motor Group's encoder position(s).
+
+        The Set Motor Position command accepts DEGREES or TURNS as valid UNITS.
+    """)
+    @act
+    def set_position(self, value: float = 0, unit: RotationUnits = DEGREES, /):  # noqa: E501
+        """Set rotation position to specified value."""
+
     @robotmesh_doc("""
         Set stopping mode of the motor by passing a brake mode as a parameter.
 
