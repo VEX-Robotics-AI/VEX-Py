e52d5f4376cf9e76ec298868a7af65e7313fe926
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-20 20:46:02 -0800

    update vex.brain.screen.BrainLcd.next_row() method

--- src/vex/brain/screen/__init__.py
+++ src/vex/brain/screen/__init__.py
@@ -67,6 +67,24 @@ class BrainLcd(SingletonDevice):
     def set_cursor(self, row: int, col: int):
         """Set the cursor location for Brain's Print commands."""
 
+    @vexcode_doc("""
+        New Line
+
+        Sets the print output cursor on the IQ Brain's screen
+        to the next available row.
+
+        By default, all projects begin with the screen cursor
+        at row 1 column 1. The New Line command will move the cursor
+        down by a single row on the screen.
+
+        The IQ (2nd generation) Brain allows you to change the size of the font
+        printed on the screen. Changing the font will affect the number of rows
+        and columns available on the Brain's screen.
+    """)
+    @act
+    def next_row(self):
+        """Move the cursor down by a single row on the screen."""
+
     @robotmesh_doc("""
         Clear the whole screen.
     """)
@@ -81,29 +99,6 @@ class BrainLcd(SingletonDevice):
     @act
     def clear_screen(self):
         """Clear Brain LCD Screen."""
-
-    @vexcode_doc("""
-        New Line
-
-        Sets the print output cursor on the IQ Brain's screen to the next available row.
-
-            brain.screen.next_row()
-
-        How To Use
-        By default, all projects begin with the screen cursor at row 1 column 1. The New Line command will move the cursor down by a single row on the screen.
-
-        The IQ (2nd generation) Brain allows you to change the size of the font printed on the screen. Changing the font will affect the number of rows and columns available on the Brain's screen.
-
-        Example
-        This example will print "First Message" and then will set the cursor to the next available row before printing "Second Message".
-
-            brain.screen.print("First Message")
-            brain.screen.next_row()
-            brain.screen.print("Second Message")
-    """)
-    @act
-    def next_row(self):
-        """Move the cursor down by a single row on the screen."""
 
     @vexcode_doc("""
         Clear Line
