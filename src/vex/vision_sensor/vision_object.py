9192f23a2a1c4b80bb31be4e907c50bf03cb8189
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-25 21:46:12 -0800

    update vex.vision_sensor.vision_object.VisionObject docstrings

--- src/vex/vision_sensor/vision_object.py
+++ src/vex/vision_sensor/vision_object.py
@@ -11,63 +11,63 @@ class VisionObject:
     """Vision Object."""
 
     @robotmesh_doc("""
-        Return the unique ID of the object.
+        Unique ID of the object.
     """)
     @property
     def id(self) -> int:
         """Return object ID."""
 
     @robotmesh_doc("""
-        Return the top left x position of the object.
+        Top-left X position of the object.
     """)
     @property
     def originX(self) -> int:
         """Return object's top-left X co-ordinate."""
 
     @robotmesh_doc("""
-        Return the top left y position of the object.
+        Top-left Y position of the object.
     """)
     @property
     def originY(self) -> int:
         """Return object's top-left Y co-ordinate."""
 
     @robotmesh_doc("""
-        Return the center x position of the object.
+        Center X position of the object.
     """)
     @property
     def centerX(self) -> int:
         """Return object's center X co-ordinate."""
 
     @robotmesh_doc("""
-        Return the center y position of the object.
+        Center Y position of the object.
     """)
     @property
     def centerY(self) -> int:
         """Return object's center Y co-ordinate."""
 
     @robotmesh_doc("""
-        Return the width of the object.
+        Width of the object.
     """)
     @property
     def width(self) -> int:
         """Return object's width."""
 
     @robotmesh_doc("""
-        Return the height of the object.
+        Height of the object.
     """)
     @property
     def height(self) -> int:
         """Return object's height."""
 
     @robotmesh_doc("""
-        Return the angle of the object.
+        Angle of the object.
     """)
     @property
     def angle(self) -> int:
         """Return object's angle."""
 
     @robotmesh_doc("""
-        Return True if vision sensor detects the object, False if not.
+        True if vision sensor detects the object, False if not.
     """)
     @property
     def exists(self) -> bool:
