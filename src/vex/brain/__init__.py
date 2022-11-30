1317c1334d33a26f7ccfc27a61920bdea5fafb98
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-29 20:28:56 -0800

    update vex.brain.Brain

--- src/vex/brain/__init__.py
+++ src/vex/brain/__init__.py
@@ -34,10 +34,10 @@ class Brain(SingletonDevice):
     def __init__(self):
         """Initialize Brain."""
         self._battery: BrainBattery = BrainBattery()
-        self._screen: BrainLcd = BrainLcd()
         self._buttonCheck: BrainButton = BrainButton(id="CHECK")
         self._buttonUp: BrainButton = BrainButton(id="UP")
         self._buttonDown: BrainButton = BrainButton(id="DOWN")
+        self._screen: BrainLcd = BrainLcd()
         self._sound: BrainSound = BrainSound()
         self._timer: BrainTimer = BrainTimer()
 
@@ -45,11 +45,6 @@ class Brain(SingletonDevice):
     def battery(self) -> BrainBattery:
         """Brain Battery."""
         return self._battery
-
-    @property
-    def screen(self) -> BrainLcd:
-        """Brain LCD."""
-        return self._screen
 
     @property
     def buttonCheck(self) -> BrainButton:
@@ -67,16 +62,21 @@ class Brain(SingletonDevice):
         return self._buttonDown
 
     @property
+    def screen(self) -> BrainLcd:
+        """Brain LCD Screen."""
+        return self._screen
+
+    @property
     def sound(self) -> BrainSound:
-        """Brain Sound."""
+        """Brain Sound Speaker."""
         return self._sound
 
     def play_sound(self, sound: SoundType = SoundType.SIREN, /):
-        """Play a sound effect."""
+        """Play sound effect."""
         self.sound.play_sound(sound)
 
     def play_note(self, octave: int = 3, note: int = 0, duration: int = 1000, /):  # noqa: E501
-        """Play a musical note."""
+        """Play musical note."""
         self.sound.play_note(octave, note, duration)
 
     @property
