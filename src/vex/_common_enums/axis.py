bcc57fe1ef14a1b011ed74a6cc0171f99d57e4d5
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-01-26 15:44:01 -0800

    use LiteralString in vex._common_enums

--- src/vex/_common_enums/axis.py
+++ src/vex/_common_enums/axis.py
@@ -3,11 +3,12 @@
 
 from collections.abc import Sequence
 from enum import IntEnum
+from typing import LiteralString
 
 from .._util.doc import robotmesh_doc
 
 
-__all__: Sequence[str] = 'AxisType', 'XAXIS', 'YAXIS', 'ZAXIS'
+__all__: Sequence[LiteralString] = 'AxisType', 'XAXIS', 'YAXIS', 'ZAXIS'
 
 
 @robotmesh_doc("""
--- src/vex/_common_enums/rotation.py
+++ src/vex/_common_enums/rotation.py
@@ -3,16 +3,18 @@
 
 from collections.abc import Sequence
 from enum import IntEnum
+from typing import LiteralString
 
 from .._util.doc import robotmesh_doc
 
 
-__all__: Sequence[str] = 'RotationUnits', 'DEGREES', 'TURNS'
+__all__: Sequence[LiteralString] = 'RotationUnits', 'DEGREES', 'TURNS'
 
 
 @robotmesh_doc("""
     The measurement units for rotation values.
 
+    Robot Mesh VEX IQ Python B:
     robotmesh.com/studio/content/docs/vexiq-python_b/html/classvex_1_1_rotation_units.html
 """)
 class RotationUnits(IntEnum):
