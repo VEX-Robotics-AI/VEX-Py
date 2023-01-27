69a0d1be90eb27497ce30feade5716caef861857
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-01-26 16:46:43 -0800

    use LiteralString in vex.optical_sensor

--- src/vex/optical_sensor/gesture.py
+++ src/vex/optical_sensor/gesture.py
@@ -4,9 +4,10 @@
 from collections.abc import Sequence
 from dataclasses import dataclass
 from enum import IntEnum, auto
+from typing import LiteralString
 
 
-__all__: Sequence[str] = 'GestureType', 'GestureInfo'
+__all__: Sequence[LiteralString] = 'GestureType', 'GestureInfo'
 
 
 class GestureType(IntEnum):
