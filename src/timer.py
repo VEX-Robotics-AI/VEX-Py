36cdcd8987b8f1e45a1a17c8a2ed4e9e35ca62b9
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-01-26 21:53:14 -0800

    use Self type in timer

--- src/timer.py
+++ src/timer.py
@@ -26,18 +26,18 @@ class Timer:
     @robotmesh_doc("""
         Create the Timer class, does not start the timer.
     """)
-    def __init__(self):
+    def __init__(self: Self):
         """Initialize Timer."""
 
-    def __eq__(self, other: Self) -> bool:
+    def __eq__(self: Self, other: Self) -> bool:
         """Check equality."""
         return isinstance(other, type(self))
 
-    def __hash__(self) -> int:
+    def __hash__(self: Self) -> int:
         """Return integer hash."""
         return 0
 
-    def __repr__(self) -> LiteralString:
+    def __repr__(self: Self) -> LiteralString:
         """Return string representation."""
         return type(self).__name__
 
@@ -45,35 +45,35 @@ class Timer:
         Reset the time, clearing all data.
     """)
     @act
-    def reset(self):
+    def reset(self: Self):
         """Reset."""
 
     @robotmesh_doc("""
         Start or resume timing the current lap.
     """)
     @act
-    def start(self):
+    def start(self: Self):
         """Start or resume."""
 
     @robotmesh_doc("""
         Stop or pause timing the current lap.
     """)
     @act
-    def stop(self):
+    def stop(self: Self):
         """Stop or pause."""
 
     @robotmesh_doc("""
         Stops the current lap and starts a new one.
     """)
     @act
-    def start_lap(self):
+    def start_lap(self: Self):
         """Start new lap."""
 
     @robotmesh_doc("""
         True if timer is currently running.
     """)
     @sense
-    def is_running(self) -> bool:
+    def is_running(self: Self) -> bool:
         """Check if Timer is running."""
 
     @robotmesh_doc("""
@@ -82,14 +82,14 @@ class Timer:
         Negative indices (from the end) supported. Time in seconds.
     """)
     @sense
-    def elapsed_time(self, lap_index: int = -1, /) -> float:
+    def elapsed_time(self: Self, lap_index: int = -1, /) -> float:
         """Return elapsed time."""
 
     @robotmesh_doc("""
         Number of laps stored (including the current one).
     """)
     @sense
-    def lap_count(self) -> int:
+    def lap_count(self: Self) -> int:
         """Return number of laps."""
 
     @robotmesh_doc("""
@@ -98,7 +98,7 @@ class Timer:
         Time in seconds.
     """)
     @sense
-    def total_time(self) -> float:
+    def total_time(self: Self) -> float:
         """Return total time."""
 
     @robotmesh_doc("""
@@ -107,5 +107,5 @@ class Timer:
         Time in seconds.
     """)
     @sense
-    def average_time(self) -> float:
+    def average_time(self: Self) -> float:
         """Return average time per lap."""
