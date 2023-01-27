acd4742193320d282dc627bf0c7e0fec557e7ae1
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-01-26 20:13:27 -0800

    use LiteralString for vex.controller

--- src/vex/controller/button.py
+++ src/vex/controller/button.py
@@ -22,9 +22,9 @@ __all__: Sequence[LiteralString] = ('ControllerButton',)
 class ControllerButton:
     """Controller Button."""
 
-    def __init__(self, mask: str):
+    def __init__(self, mask: LiteralString):
         """Initialize Controller Button."""
-        self.mask: str = mask
+        self.mask: LiteralString = mask
 
     def __eq__(self, other: Self) -> bool:
         """Check equality."""
