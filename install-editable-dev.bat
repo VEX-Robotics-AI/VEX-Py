e934abc2c3b7d98751037c63301ad95af925c993
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-03-02 23:34:23 -0800

    update install-editable-dev scripts

--- install-editable-dev.bat
+++ install-editable-dev.bat
@@ -1,4 +1,4 @@
 python -m ensurepip
 python -m pip install PEP517 PIP SetUpTools Wheel --upgrade
 
-python -m pip install -e ".[build, doc, lint, publish, test]" --upgrade --user
+python -m pip install -e ".[build, dev, doc, lint, publish, test]" --upgrade --user
