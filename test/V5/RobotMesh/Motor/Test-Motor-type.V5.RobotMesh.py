aaa24cd85a510bcc307a4ca71fcd71d68a322036
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-07-03 15:06:34 -0700

    add test/V5/RobotMesh/Motor/Test-Motor-type.V5.RobotMesh program

---
+++ test/V5/RobotMesh/Motor/Test-Motor-type.V5.RobotMesh.py
@@ -0,0 +1,25 @@
+"""robotmesh.com/studio/64a33791a3776a661acced69."""
+
+
+from vex import (
+    Brain, Motor,
+    V5DeviceType, Ports,
+    SECONDS,
+    wait,
+)
+
+
+brain = Brain()
+
+motor = Motor(Ports.PORT1)
+
+
+motor_type = motor.type()
+assert isinstance(motor_type, int)
+assert motor_type == V5DeviceType.MOTOR
+
+
+brain.screen.print_line(1, motor_type)
+
+
+wait(3, SECONDS)
