78b32946be9d6f9c05a07c61f60c0e81050b2e66
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-07-03 17:41:48 -0700

    update vex.motor.torque.TorqueUnits

--- src/vex/motor/torque.py
+++ src/vex/motor/torque.py
@@ -2,8 +2,8 @@
 
 
 from collections.abc import Sequence
-from enum import IntEnum, auto
-from typing import LiteralString
+from enum import IntEnum
+from typing import Literal, LiteralString
 
 from .._common_enums.percent import PercentUnits, PERCENT
 
@@ -19,14 +19,16 @@ __all__: Sequence[LiteralString] = 'TorqueUnits', '_Torque'
 
     Robot Mesh VEX IQ Python B:
     robotmesh.com/studio/content/docs/vexiq-python_b/html/classvex_1_1_torque_units.html
+
+    Robot Mesh VEX V5 Python:
+    robotmesh.com/studio/content/docs/vexv5-python/html/classvex_1_1_torque_units.html
 """)
 class TorqueUnits(IntEnum):
     """Torque units."""
 
-    PCT: int = PERCENT
-    NM: int = auto()  # torque unit measured in Newton Meters
-    IN_LB: int = auto()  # torque unit measured in Inch Pounds
+    NM: int = 0  # torque unit measured in Newton Meters
+    IN_LB: int = 1  # torque unit measured in Inch Pounds
 
 
 class _Torque(_MeasurementWithUnitABC):
-    unit: PercentUnits | TorqueUnits = PERCENT
+    unit: TorqueUnits | PercentUnits | Literal[PERCENT] = PERCENT
