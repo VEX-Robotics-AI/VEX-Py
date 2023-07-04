f7a0a7bfeea34b28cf132092f0e68a6d28271765
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-07-03 17:23:51 -0700

    update vex._util.measurement_with_unit._MeasurementWithUnit.unit type

--- src/vex/_util/measurement_with_unit.py
+++ src/vex/_util/measurement_with_unit.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from enum import IntEnum
 from typing import Literal, LiteralString
 
-from .._common_enums.percent import PERCENT
+from .._common_enums.percent import PercentUnits, PERCENT
 from .._util.type import Num
 
 
@@ -16,4 +16,4 @@ __all__: Sequence[LiteralString] = ('_MeasurementWithUnitABC',)
 @dataclass
 class _MeasurementWithUnitABC:
     measurement: Num
-    unit: IntEnum | Literal[PERCENT]
+    unit: IntEnum | PercentUnits | Literal[PERCENT]
